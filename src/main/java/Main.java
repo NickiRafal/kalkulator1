e75f861fa350aebe2fcf6ae41191b28e99e66cf6
Nicki Rafal <nicol79@op.pl>
2023-05-25 21:36:45 +0200

    git add .

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,3 +11,28 @@ public class Main{
 
     }
 }
+
+class Calculator{
+    private int a;
+    private int b;
+
+    public int  getA(){
+        return this.a;
+    }
+    public int getB(){
+        return this.b;
+    }
+    public Calculator(int a, int b){
+        this.a = a;
+        this.b =b;
+    }
+
+    public int add(){
+        return a+b;
+    }
+
+    public int subtract(){
+        return a-b;
+    }
+
+}
